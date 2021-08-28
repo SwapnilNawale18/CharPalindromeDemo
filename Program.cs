70d7653787345eb414363cd9cc44af33072dae07
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 21:07:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CharPalindromeDemo
+{
+    class CharPalindrome
+    {
+        internal bool CheckPalindrome(char[] name)
+        {
+            int i, j, n;
+            bool palindrome = true;
+            n = name.Length;
+            for (i = 0, j = n - 1; i < n / 2; i++, j--)
+            {
+                if (name[i] != name[j])
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+            return palindrome;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i, j, n;
+            char[] name = new char[10];
+            Console.Write("Enter any string: ");
+            name = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Palindrome: " + new CharPalindrome().CheckPalindrome(name));
+        }
+    }
+}
